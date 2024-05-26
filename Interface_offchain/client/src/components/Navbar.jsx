009cc2be823eb6809e8cbcaa77aaa533447323f0
codexbeast <ozajatin9309@gmail.com>
2024-05-26 23:34:24 +0530

    records display part

--- Interface_offchain/client/src/components/Navbar.jsx
+++ Interface_offchain/client/src/components/Navbar.jsx
@@ -59,9 +59,9 @@ const Navbar = () => {
               <li>
                 <NavLink to={"/notifications"}>Notifications</NavLink>
               </li>
-              <li>
+              {/* <li>
                 <NavLink to={"/applyfordoctor"}>Apply for doctor</NavLink>
-              </li>
+              </li> */}
               <li>
                 <HashLink to={"/#contact"}>Contact Us</HashLink>
               </li>
